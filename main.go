fe01e85d632b9bf9f9b56c074506a9511d51a4d7
Joel Lawi <abecedegluu@gmail.com>
2025-11-14 17:51:07 -0500

    Test de framework Gin

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("lo logramos")
+	// fmt.Println("lo logramos")
+
+	// Crear router
+	r := gin.Default()
+
+	// Ruta simple
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Probando Gin en Backend",
+		})
+	})
+
+	// Iniciar el server
+	r.Run(":8080")
 }
