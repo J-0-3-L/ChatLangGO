50a78d78846cd9ed845a1e40c04b8c6dbf3fefc8
Joel Lawi <abecedegluu@gmail.com>
2025-11-14 18:47:03 -0500

    Probando CORS en Hoppscotch

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +11,21 @@ func main() {
 	// Crear router
 	r := gin.Default()
 
+	// Uso de CORS
+	r.Use(cors.Default()) // All origins allowed by default
+
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"*"}, // Permite todos los orígenes (puedes especificar dominios en lugar de "*")
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// }))
+
 	// Ruta simple
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Probando Gin en Backend",
+			"message": "buenas mundo te presento ...",
 		})
 	})
 
