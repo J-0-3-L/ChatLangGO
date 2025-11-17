67ae9414c1eac8e5e782482f30e1fcd5461c0b0c
Joel Lawi <abecedegluu@gmail.com>
2025-11-16 19:07:00 -0500

    Config la db(sqlite3)

---
+++ internal/config/configDB.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"go000/internal/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectionDB() {
+	var err error
+
+	DB, err = gorm.Open(sqlite.Open("Chatdb.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error al conectar la DB", err)
+	}
+
+	fmt.Println("Conexion a la DB exitosa")
+}
+
+// Migracion de los modelos
+func MigrateModels() error {
+	modelsList := []interface{}{
+		&models.User{},
+	}
+
+	for _, model := range modelsList {
+		if err := DB.AutoMigrate(model); err != nil {
+			log.Fatal("Error al realizar la migracion de los modelos:", err)
+		}
+	}
+	fmt.Println("Migraciones realizadas con exito")
+	return nil
+}
