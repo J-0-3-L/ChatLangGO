208e5c4808325953fe9f51af6f2e0e30a59212fb
Joel Lawi <abecedegluu@gmail.com>
2025-11-18 21:52:08 -0500

    Add gorm a models

--- internal/models/User.go
+++ internal/models/User.go
@@ -2,9 +2,13 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type User struct {
+	gorm.Model
+
 	ID        uint   `json:"id" gorm:"primaryKey"`
 	Username  string `json:"username" gorm:"unique;not null"`
 	Email     string `json:"email" gorm:"unique;not null"`
