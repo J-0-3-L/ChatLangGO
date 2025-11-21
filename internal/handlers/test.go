439c4b93650375c203df4fef59da06bb9bf7bebf
Joel Lawi <abecedegluu@gmail.com>
2025-11-20 21:36:45 -0500

    Arreglos en la doc de la ruta

--- internal/handlers/test.go
+++ internal/handlers/test.go
@@ -4,23 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Test godoc
+// @Summary Endpoint de prueba validada
+// @Description Devuelve un mensaje simple para verificar el funcionamiento del servidor.
+// @Tags Test
+// @Produce json
+// @Success 200 {object} map[string]string
+// @Router /test/ [get]
+func Test(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Usando nueva ubicacion de rutas",
+	})
+}
+
 func RegisterRoutes(r *gin.Engine) {
 	allRoutes := r.Group("/test")
 	{
 		allRoutes.GET("/", Test)
 	}
-}
-
-// @Summary Obtener usuario por ID
-// @Description Obtiene los detalles de un usuario especificando su ID
-// @Accept json
-// @Produce json
-// @Param id path int true "ID del usuario"
-// @Success 200 {object} User
-// @Failure 404 {object} ErrorResponse
-// @Router /user/{id} [get]
-func Test(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Usando nueva ubicacion de rutas",
-	})
 }
