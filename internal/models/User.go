cb7895273b59e6d72d69a9b856b91adcca12a1d0
Joel Lawi <abecedegluu@gmail.com>
2025-11-21 18:42:24 -0500

    Simplificando User

--- internal/models/User.go
+++ internal/models/User.go
@@ -1,20 +1,14 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
+	gorm.Model //Aqui viene integrado ID,cretedAt,UpdateAt
 
-	ID        uint   `json:"id" gorm:"primaryKey"`
 	Username  string `json:"username" gorm:"unique;not null"`
 	Email     string `json:"email" gorm:"unique;not null"`
-	Password  string `json:"password" gorm:"unique;not null"`
+	Password  string `json:"-"`
 	AvatarURL string `json:"avatar_url"`
-
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
 }
