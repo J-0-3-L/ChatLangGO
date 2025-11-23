0ddac926a4f2dd05f359d08793951849b394f462
Joel Lawi <abecedegluu@gmail.com>
2025-11-22 23:08:15 -0500

    Created de dto para controller

---
+++ internal/auth/dto.go
@@ -0,0 +1,20 @@
+package auth
+
+type RegisterInput struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	//Password  string `json:"password"`
+	AvatarURL string `json:"avatar_url"`
+}
