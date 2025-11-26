fe327c6e31f2c8fe619ebcfb1a6a77ec4c148b25
Joel Lawi <abecedegluu@gmail.com>
2025-11-25 22:04:18 -0500

    Add nueva DTO de allPosts

--- internal/posts/dto.go
+++ internal/posts/dto.go
@@ -1,5 +1,11 @@
 package posts
 
+// import "go000/internal/models"
+
+// type AllPostsResponse struct {
+// 	Posts []models.Post `json:"posts"`
+// }
+
 type CreatePostInput struct {
 	Content string `json:"content" binding:"required"`
 }
