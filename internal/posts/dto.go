1e3e0c35906c658743f507b962259a4adcc1d9e9
Joel Lawi <abecedegluu@gmail.com>
2025-11-28 20:42:30 -0500

    Add news dtos para route getPostsUser

--- internal/posts/dto.go
+++ internal/posts/dto.go
@@ -1,6 +1,6 @@
 package posts
 
-// import "go000/internal/models"
+import "time"
 
 // type AllPostsResponse struct {
 // 	Posts []models.Post `json:"posts"`
@@ -14,4 +14,17 @@ type PostResponse struct {
 	ID      uint   `json:"id"`
 	Content string `json:"content"`
 	UserID  uint   `json:"user_id"`
+}
+
+type UserPostsByID struct {
+	ID        uint   `json:"id"`
+	Username  string `json:"username"`
+	AvatarURL string `json:"avatar_url"`
+}
+
+type UserPostResponse struct {
+	ID        uint          `json:"id"`
+	Content   string        `json:"content"`
+	CreatedAt time.Time     `json:"created_at"`
+	User      UserPostsByID `json:"user"`
 }
